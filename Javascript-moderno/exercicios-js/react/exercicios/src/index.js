c5599977409152b0fcd25cc0554982c21af5c2c0
MarcondesOliveira <marcondescurso@outlook.com>
2018-06-14 00:41:57 -0300

    commit react 02

--- Javascript-moderno/exercicios-js/react/exercicios/src/index.js
+++ Javascript-moderno/exercicios-js/react/exercicios/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(
     <div>
-        <Saudacao tipo="Bom dia," nome="Troll" />
+        <Pai nome="Troll" sobrenome="Face">
+            {/* Como passo os componentes Filhos aqui? */}
+            <Filho nome="Pedro" />
+            <Filho nome="Paulo" />
+            <Filho nome="Carla" />
+        </Pai>
     </div>
     , document.getElementById('root'))
